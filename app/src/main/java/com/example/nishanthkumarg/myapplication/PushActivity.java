72e51f72fdbe4ec5db07375632aa5333b8050aba
sandeeppadala <spadala88@gmail.com>
2018-08-29 12:37:43 +0530

    testing

--- app/src/main/java/com/example/nishanthkumarg/myapplication/PushActivity.java
+++ app/src/main/java/com/example/nishanthkumarg/myapplication/PushActivity.java
@@ -28,6 +28,7 @@ public class PushActivity extends AppCompatActivity {
         setContentView(R.layout.activity_push);
         setContentView(R.layout.activity_push);
         setContentView(R.layout.activity_push);
+        setContentView(R.layout.activity_push);
 
     }
 
