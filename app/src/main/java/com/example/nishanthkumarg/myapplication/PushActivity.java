2194e428f5283a49a301044aa7cd6c434487eb07
sandeeppadala <spadala88@gmail.com>
2019-03-22 17:10:02 +0530

    inteligetest commit

--- app/src/main/java/com/example/nishanthkumarg/myapplication/PushActivity.java
+++ app/src/main/java/com/example/nishanthkumarg/myapplication/PushActivity.java
@@ -32,8 +32,8 @@ public class PushActivity extends AppCompatActivity {
         setContentView(R.layout.activity_push);
         setContentView(R.layout.activity_push);
         setContentView(R.layout.activity_push);
-        setContentView(R.layout.activity_push);
-        setContentView(R.layout.activity_push);
+      //  setContentView(R.layout.activity_push);
+        //setContentView(R.layout.activity_push);
        // setContentView(R.layout.activity_push);
         //setContentView(R.layout.activity_push);
         //setContentView(R.layout.activity_push);
